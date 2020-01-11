73465c4368306f7bf8f440c4b420a9310521ff83
Kaibin <diaokaibin@gmail.com>
2020-01-11 11:27:40 +0800

    update

--- app/src/main/java/com/one/netease/ui/MainActivity.java
+++ app/src/main/java/com/one/netease/ui/MainActivity.java
@@ -1,8 +1,10 @@
 package com.one.netease.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import com.one.netease.ui.xfermode.XfermodeEraserView;
+import com.one.netease.ui.xfermode.XfermodesActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new XfermodeEraserView(this));
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goTo(View view) {
+        startActivity(new Intent(this, XfermodesActivity.class));
     }
 }
