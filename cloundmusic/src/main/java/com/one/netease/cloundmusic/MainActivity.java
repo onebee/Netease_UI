9680ba22b19299cf281d8a1d35c3345e226f87cb
Kaibin <diaokaibin@gmail.com>
2020-01-26 11:50:47 +0800

    update 网易云音乐歌单

--- cloundmusic/src/main/java/com/one/netease/cloundmusic/MainActivity.java
+++ cloundmusic/src/main/java/com/one/netease/cloundmusic/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,19 +18,24 @@ import java.lang.reflect.Field;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String IMAGE_URL_MEDIUM = "http://p3.music.126.net/iRbTMHGfy-grDtx7o2T_dA==/109951164009413034.jpg?param=400y400";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         UIUtils.getInstance(this);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.more));
-        StatusBarUtil.setStateBar(this,toolbar);
+//        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.more));
+//        StatusBarUtil.setStateBar(this,toolbar);
 
 
 //        setToolbar(toolbar);
@@ -39,6 +47,111 @@ public class MainActivity extends AppCompatActivity {
 //        if (parentView != null) {
 //            parentView.setFitsSystemWindows(true);
 //        }
+
+        initView();
+        notifyData();
+
+
+    }
+
+    private void postImage() {
+//        Glide.with(this)
+//                .load(IMAGE_URL_MEDIUM)
+//                .listener(new RequestListener<String, GlideDrawable>() {
+//                    @Override
+//                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+//                        Log.i("tuch", "onException: "+e);
+//                        return false;
+//                    }
+//                    @Override
+//                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+//                        Log.i("tuch", "onResourceReady: ");
+//                        return false;
+//                    }
+//                }).override(400, 400)
+//                .into(header_image_item);
+//
+//        // "14":模糊度；"3":图片缩放3倍后再进行模糊
+//        Glide.with(this)
+//                .load(IMAGE_URL_MEDIUM)
+//                .error(R.drawable.stackblur_default)
+//                .placeholder(R.drawable.stackblur_default)
+//                .crossFade(500)
+//                .bitmapTransform(new BlurTransformation(this, 200, 3))
+//                .into(new SimpleTarget<GlideDrawable>() {
+//                    @Override
+//                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+//                        lv_header_contail.setBackground(resource);
+//                    }
+//                });
+//        Glide.with(this).load(IMAGE_URL_MEDIUM)
+//                .error(R.drawable.stackblur_default)
+//                .bitmapTransform(new BlurTransformation(this, 250, 6))// 设置高斯模糊
+//                .listener(new RequestListener<String, GlideDrawable>() {//监听加载状态
+//                    @Override
+//                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+//                        return false;
+//                    }
+//                    @Override
+//                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+//                        toolbar.setBackgroundColor(Color.TRANSPARENT);
+//                        toolbar_bg.setImageAlpha(0);
+//                        toolbar_bg.setVisibility(View.VISIBLE);
+//                        return false;
+//                    }
+//                }).into(toolbar_bg);
+    }
+
+
+    private void notifyData() {
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        music_recycler.setLayoutManager(mLayoutManager);
+        // 需加，不然滑动不流畅
+        music_recycler.setNestedScrollingEnabled(false);
+        music_recycler.setHasFixedSize(false);
+        final MusicAdapter adapter = new MusicAdapter(this);
+        adapter.notifyDataSetChanged();
+        music_recycler.setAdapter(adapter);
+    }
+
+    //
+    Toolbar toolbar;
+    ImageView toolbar_bg;
+    ImageView header_bg;
+    RecyclerView music_recycler;
+    LinearLayout lv_header_contail;
+    ImageView header_music_log;
+    ImageView header_image_item;
+    MyNestedScrollView myNestedScrollView;
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.more));
+
+
+        toolbar_bg = findViewById(R.id.toolbar_bg);
+        header_bg = findViewById(R.id.header_bg);
+        music_recycler = findViewById(R.id.music_recycler);
+        myNestedScrollView = findViewById(R.id.nsv_scrollview);
+        header_music_log = findViewById(R.id.header_music_log);
+        LinearLayout lv_header_detail = findViewById(R.id.lv_header_detail);
+        RelativeLayout rv_header_container = findViewById(R.id.rv_header_container);
+        lv_header_contail = findViewById(R.id.lv_header_contail);
+        header_image_item = findViewById(R.id.header_image_item);
+
+//        ViewCalculateUtil.setViewLayoutParam(toolbar, 1080, 164, 0, 0, 0, 0);
+//        ViewCalculateUtil.setViewLinearLayoutParam(rv_header_container,1080,770,164,0,0,0);
+//        ViewCalculateUtil.setViewLayoutParam(toolbar,1080, 164, 0, 0, 0, 0);
+//        ViewCalculateUtil.setViewLayoutParam(toolbar_bg,1080,164+UIUtils.getInstance().getSystemBarHeight(this),0,0,0,0);
+//        ViewCalculateUtil.setViewLayoutParam(header_bg, 1080, 850, 0, 0, 0, 0);
+//        ViewCalculateUtil.setViewLayoutParam(lv_header_detail, 1080, 380, 72, 0, 52, 0);
+//        ViewCalculateUtil.setViewLinearLayoutParam(header_image_item,380,380);
+//        ViewCalculateUtil.setViewLayoutParam(header_music_log,60,60,59,0,52,0);
+
+        StatusBarUtil.setStateBar(this, toolbar);
+
+
 
     }
 
