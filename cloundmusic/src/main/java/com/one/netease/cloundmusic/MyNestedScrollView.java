14d69a1fddf0acb46117878b6ba44f701885d930
Kaibin <diaokaibin@gmail.com>
2020-01-26 10:38:12 +0800

    update 网易云音乐歌单

---
+++ cloundmusic/src/main/java/com/one/netease/cloundmusic/MyNestedScrollView.java
@@ -0,0 +1,22 @@
+package com.one.netease.cloundmusic;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * @author diaokaibin@gmail.com on 2020-01-26.
+ */
+public class MyNestedScrollView extends ScrollView {
+    public MyNestedScrollView(Context context) {
+        super(context);
+    }
+
+    public MyNestedScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyNestedScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
