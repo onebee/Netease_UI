892290a58e6d0bff3faf20e69b1cf6c8263f86f6
Kaibin <diaokaibin@gmail.com>
2020-01-30 13:38:17 +0800

    update view

---
+++ svgsample/src/main/java/com/one/netease/svgsample/MapView.java
@@ -0,0 +1,151 @@
+package com.one.netease.svgsample;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import androidx.annotation.Nullable;
+import androidx.core.graphics.PathParser;
+
+/**
+ * @author diaokaibin@gmail.com on 2020-01-30.
+ */
+public class MapView extends View {
+
+    private int[] colorArray = new int[]{0xFF239BD7, 0xFF30A9E5, 0xFF80CBF1, 0xFFF88F8F};
+
+    private Context context;
+    private List<ProviceItem> itemList;
+    private Paint paint;
+
+
+    private ProviceItem select;// 选中的省份
+
+    public MapView(Context context) throws ParserConfigurationException, IOException, SAXException {
+        super(context);
+        init(context);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs) throws ParserConfigurationException, IOException, SAXException {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) throws ParserConfigurationException, IOException, SAXException {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+
+    void init(Context context) throws ParserConfigurationException, IOException, SAXException {
+        this.context = context;
+        itemList = new ArrayList<>();
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mThread.start();
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (itemList != null) {
+            canvas.save();
+            for (ProviceItem item : itemList) {
+                if (item != select) {
+
+                    item.drawItem(canvas, paint, false);
+                } else {
+                    select.drawItem(canvas, paint, true);
+
+                }
+            }
+
+
+        }
+
+    }
+
+    private Thread mThread = new Thread() {
+
+        @Override
+        public void run() {
+            try {
+                InputStream inputStream = context.getResources().openRawResource(R.raw.china);
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = null;
+
+                builder = factory.newDocumentBuilder();
+
+                Document doc = builder.parse(inputStream);
+
+                Element rootElement = doc.getDocumentElement();
+                NodeList items = rootElement.getElementsByTagName("path");
+                for (int i = 0; i < items.getLength(); i++) {
+                    Element element = (Element) items.item(i);
+                    String pathData = element.getAttribute("android:pathData");
+                    @SuppressLint("RestrictedApi") Path path = PathParser.createPathFromPathData(pathData);
+                    ProviceItem proviceItem = new ProviceItem(path);
+
+                    proviceItem.setDrawColor(colorArray[i % 4]);
+                    itemList.add(proviceItem);
+
+
+                }
+                postInvalidate();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        handleTouch(event.getX(), event.getY());
+        return super.onTouchEvent(event);
+    }
+
+
+
+    private void handleTouch(float x, float y) {
+        if (itemList == null) {
+            return;
+        }
+        ProviceItem selectItem = null;
+
+        for (ProviceItem proviceItem : itemList) {
+            if (proviceItem.isTouch(x, y)) {
+                selectItem = proviceItem;
+            }
+
+        }
+
+        if (selectItem != null) {
+            select = selectItem;
+            postInvalidate();
+        }
+    }
+
+}
