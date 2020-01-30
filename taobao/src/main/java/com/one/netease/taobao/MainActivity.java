4d2850816fd90c75dbf3d5bd3ea0187116202a4b
Kaibin <diaokaibin@gmail.com>
2020-01-30 21:18:04 +0800

    1. handle 内存泄露

--- taobao/src/main/java/com/one/netease/taobao/MainActivity.java
+++ taobao/src/main/java/com/one/netease/taobao/MainActivity.java
@@ -1,14 +1,81 @@
 package com.one.netease.taobao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "bit";
+
+    private TextView textView;
+
+    private Handler handler1 = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+
+            Log.i(TAG, "handler1 receive : " + msg);
+
+//            textView.setText(msg.obj + "  -- " + msg.what);
+
+            startActivity(new Intent(MainActivity.this,PersonalActivity.class));
+
+            return false;
+        }
+
+    });
+
+    private Handler handler2 = new Handler() {
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Log.i(TAG, "handler2 receive : " + msg);
+
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv);
+
+        test();
+    }
+
+    private void test() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Message message = new Message();
+//                message.obj = "Net163";
+//                message.what = 163;
+
+//                SystemClock.sleep(3000);
+//                 handler2.sendMessage(message);
+                message.what = 3;
+//                handler1.sendMessage(message);
+        if (handler1!=null)
+                handler1.sendMessageDelayed(message, 3000);
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler1.removeMessages(3);
+        handler1=null;
+        Log.e(TAG, "  onDestroy -- ");
     }
 }
