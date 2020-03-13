c59c141da1a923f1269490f5d5f0da380df09ad0
Kaibin <diaokaibin@gmail.com>
2020-03-13 11:30:05 +0800

    init

---
+++ player/src/main/java/com/one/player/view/BackgroundAnimationRelativeLayout.java
@@ -0,0 +1,22 @@
+package com.one.player.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * @author diaokaibin@gmail.com on 2020/3/13.
+ */
+public class BackgroundAnimationRelativeLayout extends RelativeLayout {
+    public BackgroundAnimationRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public BackgroundAnimationRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BackgroundAnimationRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ player/src/main/java/com/one/player/view/DiscView.java
@@ -0,0 +1,22 @@
+package com.one.player.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * @author diaokaibin@gmail.com on 2020/3/13.
+ */
+public class DiscView extends RelativeLayout {
+    public DiscView(Context context) {
+        super(context);
+    }
+
+    public DiscView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DiscView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
