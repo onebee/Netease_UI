80d2f4ed7e3f597f75b5b85bf1e0c2e817c69152
Kaibin <diaokaibin@gmail.com>
2020-03-16 19:00:42 +0800

    沉浸式

--- immersion/src/main/java/com/one/immersion/MainActivity.java
+++ immersion/src/main/java/com/one/immersion/MainActivity.java
@@ -1,7 +1,13 @@
 package com.one.immersion;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -14,11 +20,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        immersion();
+
+        setHeightAndPadding(this,findViewById(R.id.toolbar));
+
         findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v,"hhhh",Snackbar.LENGTH_LONG).show();
+                Snackbar.make(v, "hhhh", Snackbar.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void immersion() {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+            int visibility = window.getDecorView().getSystemUiVisibility();
+
+            // 表示布局内容全屏展示
+            visibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+            // 隐藏虚拟导航栏
+            visibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+            // 防止内容区域发生变化
+            visibility |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+            window.getDecorView().setSystemUiVisibility(visibility);
+
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        }
+
+    }
+
+
+    public int getStatusBarHeight(Context context) {
+        int redId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (redId > 0) {
+            return context.getResources().getDimensionPixelSize(redId);
+        }
+        return 0;
+    }
+
+    public void setHeightAndPadding(Context context, View view) {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height += getStatusBarHeight(context);
+
+        view.setPadding(view.getPaddingLeft(),
+                view.getPaddingTop()+getStatusBarHeight(context),
+                view.getPaddingRight(),
+                view.getBottom());
     }
 }
