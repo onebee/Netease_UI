c053146fe948a744cfaeb7769028bb987b1c6e3f
Kaibin <diaokaibin@gmail.com>
2020-05-14 10:07:30 +0800

    update

--- app/src/main/java/com/one/netease/ui/MyView/MyViewGroup.java
+++ app/src/main/java/com/one/netease/ui/MyView/MyViewGroup.java
@@ -1,6 +1,7 @@
 package com.one.netease.ui.MyView;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
@@ -14,6 +15,17 @@ public class MyViewGroup extends ViewGroup {
     public boolean dispatchTouchEvent(MotionEvent ev) {
 
         return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
     }
 
 
